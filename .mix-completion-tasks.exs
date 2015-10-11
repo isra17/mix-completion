b724b3e3855f181dd987dd6d21df73d312570539
Štefan Jarina <stefan@jarina.cz>
2015-10-11 11:45:10 +0200

    remove Enum.filter on Mix.Task.hidden?
    Mix.Task.hidden? is deprecated

--- .mix-completion-tasks.exs
+++ .mix-completion-tasks.exs
@@ -3,7 +3,6 @@
 Mix.Task.load_all
 
 Mix.Task.all_modules
-|>  Enum.filter(fn(x) -> not Mix.Task.hidden?(x) end)
 |>  Enum.sort
 |>  Enum.map_join(" ", fn(x) -> Mix.Task.task_name(x) end)
 |>  IO.write
